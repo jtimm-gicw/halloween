3d8e56d65701d69f85b7bc329eda14966a3c2525
Jason <jtimm@gicw.org>
2024-10-14 10:44:09 -0700

    made more corrects, hopefully the last

--- apps.js
+++ apps.js
@@ -1,32 +1,27 @@
-'strict use'
-
-
-
 window.onload = function() {
     // Prompt the user with a question
     let userInput = prompt("What is your favorite scary movie?", "Friday the 13th");
 
     // Check if the userInput is not null (i.e., they didn't cancel the prompt)
     if (userInput) {
-        // Set the innerHTML of the paragraph to the user's input
         document.getElementById('movie-response').innerHTML = 'Wow! ' + userInput + ' is really scary!';
     } else {
-        // If the user didn't enter anything, display a default message
         document.getElementById('movie-response').innerHTML = 'You didn’t enter a movie!';
     }
-}
 
-const image = document.getElementById('shakableImage');
+    // Select all images with the class 'shakableImage'
+    const images = document.querySelectorAll('.shakableImage');
 
-// Add event listener for when the mouse enters the image (hover effect)
-image.addEventListener('mouseenter', function() {
-  // Add the shake class
-  image.classList.add('shake');
+    // Add event listeners for each image
+    images.forEach(image => {
+        image.addEventListener('mouseenter', function() {
+            // Add the shake class
+            image.classList.add('shake');
 
-  // Rms in this case)
-  setTimeout(function() {
-    image.classList.remove('shake');
-  }, 500);
-});
-
-
+            // Remove the shake class after the animation is done (500ms in this case)
+            setTimeout(function() {
+                image.classList.remove('shake');
+            }, 500);
+        });
+    });
+};
